4fff8725ae40590c83a464d0e84e667d858690a5
James Smith <jecs@imperial.ac.uk>
2016-10-18 14:01:38 +0100

    Corrected the index.js file.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var lexers = {
   'BNFLexer': require('./lib/bnf/lexer'),
-  'BasicLexer': require('./lib/basic/lexxer'),
+  'BasicLexer': require('./lib/basic/lexer'),
   'GallinaLexer': require('./lib/gallina/lexer'),
   'FlorenceLexer': require('./lib/florence/lexer'),
   'WhitespaceToken': require('./lib/common/token/whitespace')
