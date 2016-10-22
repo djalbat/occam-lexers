3125f324d934bf71e0feb6c5f4d8fb7548708032
James Smith <jecs@imperial.ac.uk>
2016-10-22 20:24:25 +0100

    Improved the Florence regular expression for unassigned tokens.

--- es6/florence/grammar.js
+++ es6/florence/grammar.js
@@ -1,6 +1,11 @@
 'use strict';
 
-var unicode_characters = `\\u{AC}\\u{2200}-\\u{22FF}\\u{1D400}-\\u{1D7FF}`,
+var not_sign = `\\u{AC}`,
+    mathematical_operators = `\\u{2200}-\\u{22FF}`,
+    supplemental_mathematical_operators = `\\u{2A00}-\\u{2AFF}`,
+    miscellaneous_technical = `\\u{2300}-\\u{23ff}`,
+    mathematical_alphanumric_symbols = `\\u{1D400}-\\u{1D7FF}`,
+    unicode_characters = `${not_sign}${mathematical_operators}${supplemental_mathematical_operators}${mathematical_alphanumric_symbols}${miscellaneous_technical}`,
     character = `[a-zA-Z<=>${unicode_characters}]`,
     unassigned = `${character}+`;
 
