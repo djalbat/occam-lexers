4dda9705efee8ef0923e062c8680aa0c07e0f15c
James Smith <jecs@imperial.ac.uk>
2016-10-22 22:15:35 +0100

    Improve the regular expressions in the Florence grammar.

--- es6/florence/grammar.js
+++ es6/florence/grammar.js
@@ -1,13 +1,25 @@
 'use strict';
 
-var not_sign = `\\u{AC}`,
+var c0_controls_and_basic_latin = `!#$%&*+-.\\/0-9<>?@A-Z\\\\^_a-z~`,
+    latin1_supplement = `\\u{A1}-\\u{FF}`,
     mathematical_operators = `\\u{2200}-\\u{22FF}`,
     supplemental_mathematical_operators = `\\u{2A00}-\\u{2AFF}`,
     miscellaneous_technical = `\\u{2300}-\\u{23ff}`,
-    mathematical_alphanumric_symbols = `\\u{1D400}-\\u{1D7FF}`,
-    unicode_characters = `${not_sign}${mathematical_operators}${supplemental_mathematical_operators}${mathematical_alphanumric_symbols}${miscellaneous_technical}`,
-    character = `[a-zA-Z<=>${unicode_characters}]`,
-    unassigned = `${character}+`;
+    mathematical_alphanumeric_symbols = `\\u{1D400}-\\u{1D7FF}`,
+    unicode_character =
+
+`
+${c0_controls_and_basic_latin}
+${latin1_supplement}
+${mathematical_operators}
+${supplemental_mathematical_operators}
+${miscellaneous_technical}
+${mathematical_alphanumeric_symbols}
+`,
+
+    unicode_characters = `[${unicode_character}]+`,
+    unassigned = unicode_characters;  ///
+
 
 var grammar = [
 
@@ -15,7 +27,7 @@ var grammar = [
 
   { keyword             : /Rule|Theorem|Lemma|Premises|Premise|Conclusion|Proof|Therefore|Suppose|Then|Hence|by/ },
 
-  { special             : /\,|\:|\;|\(|\)/ },
+  { special             : /\,|\;|\{|\}|=|\:|\(|\)/ },
 
   { unassigned          : new RegExp(`${unassigned}`, 'u') }
     
