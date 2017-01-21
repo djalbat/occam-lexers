ab26324ef13af44e0e576150dec089e03506fc3b
James Smith <jecs@imperial.ac.uk>
2017-01-21 10:58:19 +0000

    Removed the ErrorToken class from the exports.

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ var lexers = {
   'BNFLexer': require('./lib/bnf/lexer'),
   'BasicLexer': require('./lib/basic/lexer'),
   'FlorenceLexer': require('./lib/florence/lexer'),
+  'specialSymbols': require('./lib/specialSymbols'),
   'Line': require('./lib/common/line'),
   'Token': require('./lib/common/token'),
-  'ErrorToken': require('./lib/common/token/error'),
   'SelectedToken': require('./lib/common/token/selected'),
-  'SignificantToken': require('./lib/common/token/significant'),
-  'specialSymbols': require('./lib/specialSymbols')
+  'SignificantToken': require('./lib/common/token/significant')
 };
 
 module.exports = lexers;
