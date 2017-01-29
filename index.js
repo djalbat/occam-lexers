cdf8e11e2032f44eab5a1a0ce4e39ed9eefab1ae
James Smith <jecs@imperial.ac.uk>
2017-01-29 09:37:28 +0000

    Exported the error token.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict';
 
 var lexers = {
-  'Line': require('./lib/common/line'),
-  'Token': require('./lib/common/token'),
   'BNFLexer': require('./lib/bnf/lexer'),
   'BasicLexer': require('./lib/basic/lexer'),
   'FlorenceLexer': require('./lib/florence/lexer'),
   'specialSymbols': require('./lib/specialSymbols'),
+  'Line': require('./lib/common/line'),
+  'Token': require('./lib/common/token'),
+  'ErrorToken': require('./lib/common/token/error'),
   'SignificantToken': require('./lib/common/token/significant')
 };
 
