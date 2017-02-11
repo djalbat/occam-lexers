f788fb988c1e8fd6d98f9a5d1963dd4f2c1b6cc2
James Smith <jecs@imperial.ac.uk>
2017-02-11 13:02:30 +0000

    Removed the error token from the exports.

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 'use strict';
 
 var lexers = {
+  'Line': require('./lib/common/line'),
+  'Token': require('./lib/common/token'),
   'BNFLexer': require('./lib/bnf/lexer'),
   'BasicLexer': require('./lib/basic/lexer'),
   'FlorenceLexer': require('./lib/florence/lexer'),
   'specialSymbols': require('./lib/specialSymbols'),
-  'Line': require('./lib/common/line'),
-  'Token': require('./lib/common/token'),
-  'ErrorToken': require('./lib/common/token/error'),
   'StringToken': require('./lib/common/token/string'),
   'EndOfLineToken': require('./lib/common/token/endOfLine'),
   'WhitespaceToken': require('./lib/common/token/whitespace'),
