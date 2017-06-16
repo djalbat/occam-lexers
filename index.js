44dd534d1a802ebadf74b2151b635020d120ed2b
James Smith <jecs@imperial.ac.uk>
2017-06-16 19:44:43 +0100

    Removed the primitive lexer.

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var lexers = {
   'BNFLexer': require('./lib/bnf/lexer'),
   'BasicLexer': require('./lib/basic/lexer'),
   'FlorenceLexer': require('./lib/florence/lexer'),
-  'PrimitiveLexer': require('./lib/primitive/lexer'),
   'SignificantToken': require('./lib/common/token/significant'),
   'StringToken': require('./lib/common/token/significant/string'),
   'EndOfLineToken': require('./lib/common/token/significant/endOfLine'),
