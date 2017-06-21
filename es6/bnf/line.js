f7c2990e69adfb7dcb294ebcdd2b3b5928ed1c20
James Smith <jecs@imperial.ac.uk>
2017-06-21 22:54:57 +0100

    Removed end of line tokens from BNF lines.

--- es6/bnf/line.js
+++ es6/bnf/line.js
@@ -3,16 +3,12 @@
 const CommonLine = require('../common/line'),
       CommentTokens = require('./tokens/comment'),
       StringTokens = require('../common/tokens/string'),
-      WhitespaceTokens = require('../common/tokens/whitespace'),
-      EndOfLineToken = require('../common/token/significant/endOfLine');
+      WhitespaceTokens = require('../common/tokens/whitespace');
 
 class Line extends CommonLine {
   static fromContent(content, context, rules) { 
-    const line = super.fromContent(Line, content, context, rules, CommentTokens, StringTokens, WhitespaceTokens),
-          endOfLineToken = EndOfLineToken.fromLine(line);
+    const line = super.fromContent(Line, content, context, rules, CommentTokens, StringTokens, WhitespaceTokens);
 
-    line.pushToken(endOfLineToken);
-    
     return line;
   }
 }
