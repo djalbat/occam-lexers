b4ea067444f74f65f5be2c0d6d54dbab0787b5f9
James Smith <jecs@imperial.ac.uk>
2017-06-22 14:48:25 +0100

    Tiny changes to the BNF grammar.

--- es6/extendedBNF/grammar.js
+++ es6/extendedBNF/grammar.js
@@ -4,11 +4,11 @@ const grammar = [
 
   { "regularExpression": "\\/[^/]+\\/" },
 
-  { "special": "::=|\\||\\(|\\)|\\?|\\*|\\+|\\.|<NO_WHITESPACE>|<END_OF_LINE>|ε|;" },
+  { "special": "::=|\\||\\(|\\)|\\?|\\*|\\+|\\.|ε|;|<NO_WHITESPACE>|<END_OF_LINE>" },
 
   { "type": "\\[[^/]+\\]" },
 
-  { "name": "\\w+" }
+  { "name": "[\\w|~]+" }
     
 ];
 
