b595e5d6a3021a7f5d5c765cb0d02c865e97e028
James Smith <jecs@imperial.ac.uk>
2017-08-22 17:12:37 +0100

    Simplified the Florence grammar.

--- es6/florence/entries.js
+++ es6/florence/entries.js
@@ -2,11 +2,9 @@
 
 const entries = [
 
-  { "include"    : "include" },
-
   { "special"    : ",|;|⊢|=|::|:|\\[|\\]|\\{|\\}|\\(|\\)|\\.\\.\\.|\\.\\." },
 
-  { "keyword"    : "^(?:Rule|Axiom|Theorem|Lemma|Premises|Premise|Conclusion|Proof|Therefore|Suppose|Then|Hence|Types|Type|Variables|Variable|Contexts|Context|Constructors|Constructor|DependentTypes|DependentType|QualifiedMetavariables|QualifiedMetavariable|Metavariables|Metavariable|Abbreviations|Abbreviation|Object|Definition|from|by|let|for|is|not|in|defined|undefined)$" },
+  { "keyword"    : "^(?:Rule|Axiom|Theorem|Lemma|Premises|Premise|Conclusion|Proof|Therefore|Suppose|Then|Hence|Types|Type|Variables|Variable|Contexts|Context|Constructors|Constructor|DependentTypes|DependentType|QualifiedMetavariables|QualifiedMetavariable|Metavariables|Metavariable|Abbreviations|Abbreviation|Object|Definition|let|from|by)$" },
 
   { "unassigned" : "^.*$" }
 
