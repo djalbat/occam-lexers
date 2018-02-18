d48743c697a368f4a7da1de6a917aed0abcce28c
James Smith <jecs@imperial.ac.uk>
2018-02-18 10:34:53 +0000

    Exported the basic lexer again.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
+  'BasicLexer': require('./lib/bnf/basic'),
   'BNFLexer': require('./lib/bnf/lexer'),
   'PlainLexer': require('./lib/plain/lexer'),
   'MetaJSONLexer': require('./lib/metaJSON/lexer'),
