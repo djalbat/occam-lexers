2fa31d0bf03214c7de94c235d70557a0639e36d2
James Smith <jecs@imperial.ac.uk>
2018-05-26 11:10:09 +0100

    Added the getString() method to the string literal token.

--- es6/common/token/significant/stringLiteral.js
+++ es6/common/token/significant/stringLiteral.js
@@ -8,6 +8,16 @@ const { stringLiteralType } = types;
 class StringLiteralToken extends SignificantToken {
   clone(startPosition, endPosition) { return super.clone(StringLiteralToken, startPosition, endPosition); }
 
+  getString() {
+    const content = this.getContent(),
+          contentLength = content.length,
+          start = 1,
+          end = contentLength - 1 - 1,
+          string = content.substring(start, end);
+
+    return string;
+  }
+
   static fromContent(content) { return SignificantToken.fromContent(StringLiteralToken, content); }
 
   static fromWithinContent(content) { return SignificantToken.fromWithinContent(StringLiteralToken, content); }
