d1556dd97bf115bdb891314a87d28c52018d514a
James Smith <james.smith@openmathematics.org>
2018-11-01 18:39:13 +0000

    Made end of line tokens significant for CSS.

--- es6/css/entries.js
+++ es6/css/entries.js
@@ -2,9 +2,24 @@
 
 const entries = [
 
-  { "special"    : "" },
 
-  { "unassigned" : "^.*$" }
+  { "special"      : ":|;|!|\\{|\\}|\\(|\\)" },
+
+
+  { "hexColour"    : "^#(?:[0-9a-fA-F]{3}|[0-9a-fA-F]{6})$" },
+
+
+  { "functionName" : "^(?:attr|calc|cubic-bezier|hsl|hsla|linear-gradient|radial-gradient|repeating-linear-gradient|repeating-radial-gradient|rgb|rgba|var)$" },
+
+
+  { "keyword"      : "^(?:important)$" },
+
+
+  { "name"         : "^[_a-z][_a-z0-9-]*$" },
+
+
+  { "unassigned"   : "^.*$" }
+
 
 ];
 
--- es6/css/lexer.js
+++ es6/css/lexer.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const entries = require('./entries'),
-      CommonLexer = require('../common/lexer');
+      CommonLexer = require('../common/lexer'),
+      SignificantEndOfLineTokens = require('../common/tokens/endOfLine/significant');
 
 class CSSLexer extends CommonLexer {
+  tokeniseEndOfLines(tokensOrContents) { SignificantEndOfLineTokens.tokenise(tokensOrContents); }
+
   tokeniseRegularExpressions(tokensOrContents) {}
 
   static fromNothing() { return CommonLexer.fromNothing(CSSLexer); }
