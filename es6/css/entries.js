eeaf41ea23f7f1160861b5e8b46a398439bdc78f
James Smith <james.smith@openmathematics.org>
2018-11-02 18:14:42 +0000

    Improvements to the CSS entries.

--- es6/css/entries.js
+++ es6/css/entries.js
@@ -2,24 +2,60 @@
 
 const entries = [
 
-
-  { "special"      : ":|;|!|\\{|\\}|\\(|\\)" },
-
-
-  { "hexColour"    : "^#(?:[0-9a-fA-F]{3}|[0-9a-fA-F]{6})$" },
-
-
-  { "functionName" : "^(?:attr|calc|cubic-bezier|hsl|hsla|linear-gradient|radial-gradient|repeating-linear-gradient|repeating-radial-gradient|rgb|rgba|var)$" },
-
-
-  { "keyword"      : "^(?:important)$" },
-
-
-  { "name"         : "^[_a-z][_a-z0-9-]*$" },
-
-
-  { "unassigned"   : "^.*$" }
-
+  {
+    "special": ";|,|/|\\|=|~=|=|>|\\{|\\}|\\(|\\)|\\[|\\]"
+  },
+  {
+    "keyword": "(?:!important|@media)"
+  },
+  {
+    "colour": "#(?:[0-9a-fA-F]{3}|[0-9a-fA-F]{6})"
+  },
+  {
+    "percentage": "(?:[0-9]+|[0-9]*\\.[0-9]+)\\%"
+  },
+  {
+    "frequency": "(?:[0-9]+|[0-9]*\\.[0-9]+)(?:hz|khz)"
+  },
+  {
+    "length": "(?:[0-9]+|[0-9]*\\.[0-9]+)(?:px|cm|mm|in|pt|pc)"
+  },
+  {
+    "angle": "(?:[0-9]+|[0-9]*\\.[0-9]+)(?:deg|rad|grad)"
+  },
+  {
+    "time": "(?:[0-9]+|[0-9]*\\.[0-9]+)(?:s|ms)"
+  },
+  {
+    "rems": "(?:[0-9]+|[0-9]*\\.[0-9]+)rem"
+  },
+  {
+    "ems": "(?:[0-9]+|[0-9]*\\.[0-9]+)em"
+  },
+  {
+    "number": "[0-9]+|[0-9]*\\.[0-9]+"
+  },
+  {
+    "pseudoElement": "::[_a-z][_a-z0-9-]*"
+  },
+  {
+    "pseudoClass": ":[_a-z][_a-z0-9-]*"
+  },
+  {
+    "class": "\\.[_a-z][_a-z0-9-]*"
+  },
+  {
+    "operator": "^and|not|only|not$"
+  },
+  {
+    "identifier": "[_a-z][_a-z0-9-]*"
+  },
+  {
+    "unaryOperator": "\\+|\\-"
+  },
+  {
+    "unassigned": "^.*$"
+  }
 
 ];
 
