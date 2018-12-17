a29304a31f36674d097f2e373e88b04a49097e68
James Smith <james.smith@openmathematics.org>
2018-12-17 20:12:01 +0000

    Removed the custom grammar lexers.

--- es6/customGrammarLexicalPattern/entries.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const entries = [
-
-  { "unassigned" : "^.*$" }
-
-];
-
-module.exports = entries;
--- index.js
+++ index.js
@@ -11,7 +11,5 @@ module.exports = {
   'BasicLexer': require('./lib/basic/lexer'),
   'PlainLexer': require('./lib/plain/lexer'),
   'MetaJSONLexer': require('./lib/metaJSON/lexer'),
-  'FlorenceLexer': require('./lib/florence/lexer'),
-  'CustomGrammarBNFLexer': require('./lib/customGrammarBNF/lexer'),
-  'CustomGrammarLexicalPatternLexer': require('./lib/customGrammarLexicalPattern/lexer')
+  'FlorenceLexer': require('./lib/florence/lexer')
 };
