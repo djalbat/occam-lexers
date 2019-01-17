0dd84fc2a5cbd9956881187f65f3a7a27631f5a8
James Smith <james.smith@openmathematics.org>
2019-01-17 20:48:43 +0000

    Added the types to the index file.

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 'use strict';
 
 module.exports = {
-  SignificantToken: require('./lib/common/token/significant'),
-  NonSignificantToken: require('./lib/common/token/nonSignificant'),
-  SignificantEndOfLineTokens: require('./lib/common/tokens/endOfLine/significant'),
-  NonSignificantEndOfLineTokens: require('./lib/common/tokens/endOfLine/nonSignificant'),
   Rule: require('./lib/common/rule'),
   Rules: require('./lib/common/rules'),
+  types: require('./lib/common/types'),
   BNFLexer: require('./lib/bnf/lexer'),
   LaTeXLexer: require('./lib/latex/lexer'),
   BasicLexer: require('./lib/basic/lexer'),
   CommonLexer: require('./lib/common/lexer'),
-  FlorenceLexer: require('./lib/florence/lexer')
+  FlorenceLexer: require('./lib/florence/lexer'),
+  SignificantToken: require('./lib/common/token/significant'),
+  NonSignificantToken: require('./lib/common/token/nonSignificant'),
+  SignificantEndOfLineTokens: require('./lib/common/tokens/endOfLine/significant'),
+  NonSignificantEndOfLineTokens: require('./lib/common/tokens/endOfLine/nonSignificant')
 };
