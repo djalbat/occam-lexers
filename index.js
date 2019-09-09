557f92d3d1b7dfe64ac255e205d31b55dd00efbf
James Smith <james.smith@openmathematics.org>
2019-09-09 11:20:35 +0100

    Fixed the index file.

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ module.exports = {
   SignificantToken: require('./lib/common/token/significant'),
   NonSignificantToken: require('./lib/common/token/nonSignificant'),
   defaultLexicalPattern: require('./lib/florence/defaultLexicalPattern'),
+  SignificantEndOfLineTokens: require('./lib/common/tokens/endOfLine/significant'),
+  NonSignificantEndOfLineTokens: require('./lib/common/tokens/endOfLine/nonSignificant'),
   BNFLexer: require('./lib/bnf/lexer'),
   PlainLexer: require('./lib/plain/lexer'),
   LaTeXLexer: require('./lib/latex/lexer'),
