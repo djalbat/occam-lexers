d5df23305af197f0bdb14b67b0d39eb0e1787074
James Smith <james.smith@openmathematics.org>
2019-10-01 11:30:41 +0100

    Added the unassigned entry back to the Florence entries.

--- es6/florence/entries.js
+++ es6/florence/entries.js
@@ -6,7 +6,9 @@ const entries = [
 
   { "keyword"    : "^(?:Rule|Axiom|Theorem|Lemma|Metalemma|Metatheorem|Premises|Premise|Conclusion|Proof|Therefore|Suppose|Then|Hence|Types|Type|Variables|Variable|Contexts|Context|Constructors|Constructor|DependentTypes|DependentType|Metavariables|Metavariable|Abbreviations|Abbreviation|for|let|from|by)" },
 
-  { "name"       : "^[A-Za-zΑ-Ωα-ω_]+" }
+  { "name"       : "^[A-Za-zΑ-Ωα-ω_]+" },
+
+  { "unassigned" : "^[^\\s]+" }
 
 ];
 
