164e889fdc2b4fc5b440ea7124f6e12ea68da025
James Smith <james.smith@openmathematics.org>
2020-01-01 18:52:11 +0000

    Added the conjecture keyword.

--- es6/florence/entries.js
+++ es6/florence/entries.js
@@ -4,7 +4,7 @@ const entries = [
 
   { "special"    : "^(?:,|=|::|:|\\|-|\\(|\\)|\\[|\\]|\\.\\.\\.)" },
 
-  { "keyword"    : "^(?:Rule|Axiom|Theorem|Lemma|Metalemma|Metatheorem|Premises|Premise|Conclusion|Proof|Therefore|Suppose|Then|Hence|Types|Type|Variables|Variable|Contexts|Context|Constructors|Constructor|DependentTypes|DependentType|DisjointTypes|DisjointType|Metavariables|Metavariable|Abbreviations|Abbreviation|for|let|from|by)\\b" },
+  { "keyword"    : "^(?:Rule|Axiom|Theorem|Lemma|Conjecture|Metalemma|Metatheorem|Premises|Premise|Conclusion|Proof|Therefore|Suppose|Then|Hence|Types|Type|Variables|Variable|Contexts|Context|Constructors|Constructor|DependentTypes|DependentType|DisjointTypes|DisjointType|Metavariables|Metavariable|Abbreviations|Abbreviation|for|let|from|by)\\b" },
 
   { "name"       : "^[A-Za-zΑ-Ωα-ω_0-9]+" },
 
