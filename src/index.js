09dc5b2c449dda355ca7274b2504c7cdf8a64a75
James Smith <james.smith@djalbat.com>
2021-11-25 17:39:58 +0000

    Exported more tokens.

--- src/index.js
+++ src/index.js
@@ -8,8 +8,16 @@ export { default as BNFLexer } from "./bnf/lexer";
 export { default as BasicLexer } from "./basic/lexer";
 export { default as CommonLexer } from "./common/lexer";
 
-export { default as WhitespaceToken } from "./common/token/nonSignificant/whitespace";  ///
 export { default as SignificantToken } from "./common/token/significant";
 export { default as NonSignificantToken } from "./common/token/nonSignificant";
 export { default as EndOfLineSignificantToken } from "./common/token/significant/endOfLine";
 export { default as EndOfLineNonSignificantToken } from "./common/token/nonSignificant/endOfLine";
+
+export { default as WhitespaceToken } from "./common/token/nonSignificant/whitespace";  ///
+export { default as SingleLineCommentToken } from "./common/token/nonSignificant/comment/singleLine"; ///
+export { default as EndOfMultiLineCommentToken } from "./common/token/nonSignificant/comment/multiLine/endOf";  ///
+export { default as StartOfMultiLineCommentToken } from "./common/token/nonSignificant/comment/multiLine/startOf";  ///
+export { default as MiddleOfMultiLineCommentToken } from "./common/token/nonSignificant/comment/multiLine/middleOf";  ///
+export { default as SinglyQuotedStringLiteralToken } from "./common/token/significant/stringLiteral/singlyQuoted";  ///
+export { default as DoublyQuotedStringLiteralToken } from "./common/token/significant/stringLiteral/doublyQuoted";  ///
+
