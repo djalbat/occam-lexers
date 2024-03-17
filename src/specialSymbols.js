1f11deb3c8d401b6477f9f4eaa90483b25de9b5f
James Smith <james.smith@djalbat.com>
2024-03-17 11:15:12 +0000

    Changes to the special symbols.

--- src/specialSymbols.js
+++ src/specialSymbols.js
@@ -1,33 +1,35 @@
 "use strict";
 
 export const plus = "+";
+export const opaque = ".";
 export const epsilon = 'ε';
 export const wildcard = ".";
 export const asterisk = "*";
 export const ellipsis = "...";
+export const endOfLine = "<END_OF_LINE>";
 export const separator = "::=";
+export const semiOpaque = "..";
 export const terminator = ";";
 export const verticalBar = "|";
 export const openBracket = "(";
 export const closeBracket = ")";
 export const questionMark = "?";
-export const exclamationMark = "!";
-export const NO_WHITESPACE = "<NO_WHITESPACE>";
-export const END_OF_LINE = "<END_OF_LINE>";
+export const noWhitespace = "<NO_WHITESPACE>";
 
 export default {
   plus,
+  opaque,
   epsilon,
   wildcard,
   asterisk,
   ellipsis,
+  endOfLine,
   separator,
+  semiOpaque,
   terminator,
   verticalBar,
   openBracket,
   closeBracket,
   questionMark,
-  exclamationMark,
-  NO_WHITESPACE,
-  END_OF_LINE
+  noWhitespace
 };
